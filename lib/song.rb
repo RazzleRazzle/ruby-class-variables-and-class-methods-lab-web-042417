96198bf8970b8b79ba47beaba02115b63267bd2e
Jake Musich <makejusich@gmail.com>
2017-04-27 00:06:15 -0400

    8 tests passing

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre) #create new Song w. name, artist, genre, and a counter
+    @name = name
+    @@artists << "#{artist}"
+    @@genres << "#{genre}"
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+end
