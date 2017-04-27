f3915ee3bdc69b97ad8de8dfa5dd32cacc2604c5
Jake Musich <makejusich@gmail.com>
2017-04-27 09:32:20 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,11 @@ class Song
   @@genres = []
   @@artists = []
 
+
   def initialize(name, artist, genre) #create new Song w. name, artist, genre, and a counter
     @name = name
+    @artist = artist
+    @genre = genre 
     @@artists << "#{artist}"
     @@genres << "#{genre}"
     @@count += 1
@@ -26,4 +29,21 @@ class Song
   def self.artists
     @@artists.uniq
   end
+
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      genre_hash[genre] = @@genres.count(genre)
+    end
+    genre_hash
+  end
+
+  def self.artist_count
+      artist_hash = {}
+    @@artists.uniq.each do |artist|
+      artist_hash[artist] = @@artists.count(artist)
+    end
+    artist_hash
+  end
+
 end
